2ac88204caed90d6bd657360cf24a98d12241ac9
Nicolas Jasso <docnico23c9@gmail.com>
2025-05-04 19:47:57 -0600

    desde nueva rama commit

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -5,9 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
 public String hello(){
     return  "Hola mundo desde Sprint Boot by Nicolas";
 }
+    @GetMapping("/hello2")
+public String hello2(){
+        return  "Hola mundo desde 2do metodo  second push Nicolas";
+    }
 
 }
